1ca3c698d32b98540586dfef292874d28f925459
JP Martin <jpmartin@microsoft.com>
2014-07-31 18:07:45 -0700

    First shot at adding images, plots.
    Though the plots code I'm suspicious about.
    We'll want to make it more
    robust, anyways, to be able to deal with things like arrays instead of
    just PSCustomObjects.

--- Sean/Scripts/sean-insert-image.ps1
+++ Sean/Scripts/sean-insert-image.ps1
@@ -1,14 +1,13 @@
 ﻿<# 
 .SYNOPSIS
-Insert xaml as an output block
+Insert an image as an output block
 
 .DESCRIPTION
-$button = sean-insert-xaml '<Button>B</Button>'
-$button.Child.AddHandler([System.Windows.Controls.Button]::ClickEvent, [System.Windows.RoutedEventhandler]{ 1 })
+$foo = sean-insert-image http://a5.mzstatic.com/us/r30/Purple4/v4/c3/57/d5/c357d588-848b-474c-ed1b-4a636b2c33e9/icon_256.png
 
 
 #>
-param([string]$xaml, [object]$DataContext = $null)
+param([string]$imgurl, [object]$DataContext = $null)
 
 $wpfhost = $Host.PrivateData.UI -as [Sean.MyPSHostUserInterface]
 if (!$wpfhost) {
@@ -21,7 +20,7 @@ $wrapxaml = @"
     xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
     xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">
 $xaml
-<Image Source="http://a5.mzstatic.com/us/r30/Purple4/v4/c3/57/d5/c357d588-848b-474c-ed1b-4a636b2c33e9/icon_256.png"/>
+<Image Source="$imgurl"/>
 </Border>
 "@;
 
---
+++ Sean/Scripts/sean-insert-plot.ps1
@@ -0,0 +1,51 @@
+﻿<# 
+.SYNOPSIS
+Insert a plot of the data. Expects an array of PSObjects with two properties.
+
+.DESCRIPTION
+$big5 = dir | Sort-Object Length -descending | select -first 5 | select Name, Length 
+$foo = sean-insert-plot.ps1 $big5
+
+$foo = $big5 | sean-insert-plot.ps1 
+
+
+#>
+param($table, [object]$DataContext = $null)
+
+$wpfhost = $Host.PrivateData.UI -as [Sean.MyPSHostUserInterface]
+if (!$wpfhost) {
+  throw "Not a Sean shell"
+}
+
+# Build the API URL to get the plot
+$theproperties = $table[0].psobject.properties | Foreach { $_.Name }
+$thelabels=$()
+if ($theproperties.Length -le 1) {
+	$thevalues = $table | ForEach { $_.($theproperties[0]) }
+} else {
+    $thelabels = $table | ForEach { $_.($theproperties[0]) }
+	$thevalues = $table | ForEach { $_.($theproperties[1]) }
+}
+
+$prettyval = $thevalues -join ','
+$prettylabels = $thelabels -join '|'
+
+$ploturl="https://chart.googleapis.com/chart?chs=500x100&chd=t:${prettyval}&cht=p"
+if ($thelabels.Length -gt 0) {
+  $ploturl += "&chl=${prettylabels}"
+}
+
+# XAML doesn't seem to like the "=" character in URLs (?)
+$ploturl = $ploturl -replace '&','&amp;'
+
+# wrap with border mainly to get the namespaces in place
+$wrapxaml = @"
+<Border 
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">
+$xaml
+<Image Stretch="None" Source="$ploturl"/>
+</Border>
+"@;
+
+$wpfhost.SeanInsertXaml($wrapxaml, $DataContext);
